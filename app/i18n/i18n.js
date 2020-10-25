f425da93fd5cf564ad57f16973b201e2136f1606
etsraphael <ets_raphael@outlook.fr>
2020-10-25 16:58:43 +1000

    minors update

--- app/i18n/i18n.js
+++ app/i18n/i18n.js
@@ -1,12 +1,11 @@
-import I18n from 'react-native-i18n';
-import en from './locales/en.json';
-import fr from './locales/fr.json';
+// import en from './locales/en.json';
+// import fr from './locales/fr.json';
 
-I18n.fallbacks = true;
+// I18n.fallbacks = true;
  
-I18n.translations = {
-  en,
-  fr
-}
+// I18n.translations = {
+//   en,
+//   fr
+// }
 
-export default I18n
+// export default I18n
