a7a8fca740013b7165a678e1ffaf605cf8078b11
etsraphael <ets_raphael@outlook.fr>
2020-11-28 23:03:51 +1000

    update with profile updated

--- app/components/space/profile/my-profile.js
+++ app/components/space/profile/my-profile.js
@@ -8,7 +8,6 @@ import ProfileMusic from './profile-music'
 import * as ProfilePublicationActions from '../../../../redux/ProfilePublications/actions'
 import * as MusicProjectListActions from '../../../../redux/MusicProjectList/actions'
 import * as MyUserActions from '../../../../redux/MyUser/actions'
-import * as PublicationFeedActions from '../../../../redux/FeedPublications/actions'
 import LinearGradient from 'react-native-linear-gradient'
 import ActionSheet from 'react-native-actionsheet'
 import AsyncStorage from '@react-native-community/async-storage';
@@ -22,10 +21,6 @@ class MyProfile extends React.Component {
         this.state = {
             space: 'feed'
         }
-    }
-
-    componentWillUnmount() {
-        // this.props.actions.getByMode(1, 'FollowerAndFriend')
     }
 
     componentDidMount() {
@@ -60,29 +55,29 @@ class MyProfile extends React.Component {
                 <View style={{ flex: 1, position: 'relative' }}>
 
                     {/* Back Btn */}
-                    <TouchableOpacity  onPress={() => this.props.screenProps.rootNavigation.goBack(null)}
-                        style={{ position: 'absolute', left: 25, width: 35, height: 35, top: 55, zIndex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <TouchableOpacity onPress={() => this.props.screenProps.rootNavigation.goBack(null)}
+                        style={{ position: 'absolute', left: 25, width: 35, height: 35, top: 55, zIndex: 1, justifyContent: 'center', alignItems: 'center' }}>
                         <FontAwesomeIcon icon={faArrowLeft} color={'white'} size={30} />
                     </TouchableOpacity>
 
                     {/* Dropdown Btn */}
-                    <TouchableOpacity onPress={this.showActionSheet} 
-                    style={{position: 'absolute', right: 25, width: 35, height: 35, top: 55, zIndex: 1}}>
+                    <TouchableOpacity onPress={this.showActionSheet}
+                        style={{ position: 'absolute', right: 25, width: 35, height: 35, top: 55, zIndex: 1 }}>
                         <FontAwesomeIcon icon={faUserCog} color={'white'} size={30} />
                     </TouchableOpacity>
 
                     {/* Cover Picture */}
                     <FastImage
-                        style={{ height: 300 }}
+                        style={{ height: 200 }}
                         resizeMode={FastImage.resizeMode.cover}
                         source={{ uri: this.props.MyProfile.profile.picturecover, priority: FastImage.priority.normal }}
                     />
 
                     {/* Background Filter */}
-                    <View style={{ backgroundColor: '#0000004d', width: '100%', height: 300, position: 'absolute' }} />
+                    <View style={{ backgroundColor: '#0000004d', width: '100%', height: 200, position: 'absolute' }} />
 
                     {/* Profile picture and name */}
-                    <View style={{ position: 'absolute', top: 210, width: '100%', flexDirection: 'row', paddingHorizontal: 5 }}>
+                    <View style={{ position: 'absolute', top: 110, width: '100%', flexDirection: 'row', paddingHorizontal: 5 }}>
                         <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
                             <FastImage
                                 style={{ height: 66, width: 66, borderRadius: 66 }}
@@ -95,61 +90,27 @@ class MyProfile extends React.Component {
                         </View>
                         <View style={{ flex: 7, paddingLeft: 5, justifyContent: 'center' }}>
                             <Text style={{ fontSize: 22, color: 'white', fontFamily: 'Avenir-Heavy' }}>@{this.props.MyProfile.profile._meta.pseudo}</Text>
+                            <Text style={{ color: 'white', fontSize: 15, fontFamily: 'Avenir-Book' }}>Community : {this.props.MyProfile.profile.communityTotal}</Text>
                         </View>
-                    </View>
-
-                    {/* Stat */}
-                    <View style={{ flexDirection: 'row', padding: 15, paddingRight: 15, borderColor: '#9d9d9d', borderBottomWidth: 0.5 }}>
-
-
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-
-                            <View style={{ flex: 1, alignItems: 'center' }}>
-                                <Text style={{ color: '#7C7C7C', fontSize: 17, fontFamily: 'Avenir-Heavy' }}>3121</Text>
-                                <Text style={{ color: '#4E586E', fontSize: 15, fontFamily: 'Avenir-Book' }}>Following</Text>
-                            </View>
-                            <View style={{ flex: 1, alignItems: 'center' }}>
-                                <Text style={{ color: '#7C7C7C', fontSize: 17, fontFamily: 'Avenir-Heavy' }}>5024</Text>
-                                <Text style={{ color: '#4E586E', fontSize: 15, fontFamily: 'Avenir-Book' }}>Followers</Text>
-                            </View>
-
-                        </View>
-
-
-                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
-
-                            <LinearGradient
-                                start={{ x: 0, y: 1 }}
-                                end={{ x: 1, y: 0 }}
-                                colors={['#3C349B', '#8E46DF']}
-                                style={{ borderRadius: 25, position: 'relative', bottom: 3 }}
-                            >
-                                <TouchableOpacity >
-                                    <Text style={styles.title_btn_nav}>Setting</Text>
-                                </TouchableOpacity>
-                            </LinearGradient>
-
-                        </View>
-
                     </View>
 
                     {/* Nav */}
-                    <View style={{ flexDirection: 'row', padding: 15, paddingRight: 15, borderColor: '#9d9d9d', borderBottomWidth: 0.5 }}>
+                    <View style={{ flexDirection: 'row', padding: 15 }}>
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <FontAwesomeIcon icon={faNewspaper} color={'grey'} size={25} style={{ opacity: 0.8 }} />
+                            <FontAwesomeIcon icon={faNewspaper} color={'grey'} size={25} style={{ opacity: 0.8 }} />
                         </View>
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <FontAwesomeIcon icon={faVideo} color={'grey'} size={25} style={{ opacity: 0.8 }} />
+                            <FontAwesomeIcon icon={faVideo} color={'grey'} size={25} style={{ opacity: 0.8 }} />
                         </View>
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <FontAwesomeIcon icon={faMusic} color={'grey'} size={25} style={{ opacity: 0.8 }} />
+                            <FontAwesomeIcon icon={faMusic} color={'grey'} size={25} style={{ opacity: 0.8 }} />
                         </View>
                     </View>
 
                 </View>
-           
-           
-           
+
+
+
             </View>
         )
     }
