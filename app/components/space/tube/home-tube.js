9fbc754650d95a02e33d1458a44cd29b00823d9d
etsraphael <ets_raphael@outlook.fr>
2020-12-07 16:29:09 +1000

    code cleaned

--- app/components/space/tube/home-tube.js
+++ app/components/space/tube/home-tube.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native'
 import { connect } from 'react-redux'
 import * as MyUserActions from '../../../../redux/MyUser/actions'
 import { bindActionCreators } from 'redux'
