0c26688090bd36d19ab39d9c18049fe3b565af21
etsraphael <ets_raphael@outlook.fr>
2021-02-02 13:03:37 +1000

    update with setting design

--- app/components/space/setting/setting.js
+++ app/components/space/setting/setting.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux'
 import * as MyUserActions from './../../../../redux/MyUser/actions'
 import { bindActionCreators } from 'redux'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faNewspaper, faMusic, faVideo, faArrowLeft, faUserCog } from '@fortawesome/pro-light-svg-icons'
-import FastImage from 'react-native-fast-image'
+import { faArrowLeft, faKey, faUser, faLanguage, faEllipsisH, faWallet, faCertificate, faSignOut } from '@fortawesome/pro-light-svg-icons'
+import LinearGradient from 'react-native-linear-gradient'
 
 class Setting extends React.Component {
 
@@ -15,9 +15,8 @@ class Setting extends React.Component {
 
     // to display the header of the profile
     _renderHeader = () => {
-
         return (
-            <View style={styles.header_container}>
+            <View style={{ borderBottomRightRadius: 35, backgroundColor: '#0C1A32', height: 200, overflow: 'hidden' }}>
 
                 <View style={{ flex: 1, position: 'relative' }}>
 
@@ -28,31 +27,92 @@ class Setting extends React.Component {
                     </TouchableOpacity>
 
                     {/* Cover Picture */}
-                    <FastImage
-                        style={{ height: 200 }}
-                        resizeMode={FastImage.resizeMode.cover}
-                        source={{ uri: this.props.MyProfile.profile.picturecover, priority: FastImage.priority.normal }}
-                    />
 
-                    {/* Background Filter */}
-                    <View style={{ backgroundColor: '#0000004d', width: '100%', height: 200, position: 'absolute' }} />
+                    <LinearGradient
+                        colors={['#f12711', '#f5af19']}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 1 }}
+                        style={{ height: 200 }} />
 
                     {/* Profile picture and name */}
-                    <View style={{ position: 'absolute', top: 110, width: '100%', flexDirection: 'row', paddingHorizontal: 5 }}>
-                        <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                            <FastImage
-                                style={{ height: 66, width: 66, borderRadius: 66 }}
-                                source={{
-                                    uri: this.props.MyProfile.profile.pictureprofile,
-                                    priority: FastImage.priority.normal,
-                                }}
-                                resizeMode={FastImage.resizeMode.cover}
-                            />
-                        </View>
-                        <View style={{ flex: 7, paddingLeft: 5, justifyContent: 'center' }}>
-                            <Text style={{ fontSize: 22, color: 'white', fontFamily: 'Avenir-Heavy' }}>@{this.props.MyProfile.profile._meta.pseudo}</Text>
+                    <View style={{ position: 'absolute', top: 130, width: '100%', flexDirection: 'row', paddingHorizontal: 5 }}>
+                        <View style={{ flex: 1, paddingHorizontal: 35, justifyContent: 'center' }}>
+                            <Text style={{ fontSize: 28, color: 'white', fontFamily: 'Avenir-Heavy' }}>Setting</Text>
                         </View>
                     </View>
+                </View>
+            </View>
+        )
+    }
+
+    // icons setting
+    _renderBody = () => {
+        return (
+            <View style={{ flex: 1, padding: 15 }}>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faUser} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Profile</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faKey} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Password</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faLanguage} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Language</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faWallet} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Ledger</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faCertificate} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Certification</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faEllipsisH} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Others</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={{ flex: 1, padding: 15, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.onCard}>
+                            <FontAwesomeIcon icon={faSignOut} color={'#808080a3'} size={30} />
+                        </View>
+                        <View style={{ paddingTop: 8 }}>
+                            <Text style={{ color: '#000000', fontSize: 16, fontWeight: '600', fontFamily: 'Avenir-Heavy' }}>Log Out</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={{ flex: 1 }} />
+                    <View style={{ flex: 1 }} />
                 </View>
             </View>
         )
@@ -62,9 +122,9 @@ class Setting extends React.Component {
 
         return (
             <View style={styles.container}>
-                <ScrollView>
+                <ScrollView style={{ height: '100%' }}>
                     {this._renderHeader()}
-                    {/* {this._renderBody()} */}
+                    {this._renderBody()}
                 </ScrollView>
             </View>
         )
@@ -74,6 +134,22 @@ class Setting extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
+    },
+    onCard: {
+        backgroundColor: 'white',
+        borderRadius: 9,
+        padding: 35,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
     }
 })
 
