6eb098e648c4d0ca2463e09cc73b5919901f780d
etsraphael <ets_raphael@outlook.fr>
2021-03-02 15:28:51 +1000

    update - modal report designed

--- app/components/core/modal/option-publication-modal.js
+++ app/components/core/modal/option-publication-modal.js
@@ -1,21 +1,8 @@
 import React from 'react'
-import {
-    StyleSheet, View, Text, TouchableOpacity, Image,
-    ActivityIndicator, KeyboardAvoidingView, Platform, FlatList, Keyboard, TextInput
-} from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Modal from 'react-native-modal'
-import FastImage from 'react-native-fast-image'
-import LinearGradient from 'react-native-linear-gradient'
-import TagSuggest from '../tag-suggest'
-import Video from 'react-native-video'
-import { getDateTranslated } from '../../../services/translation/translation-service'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCommentLines, faHeart as faHeartEmpty, faPaperPlane } from '@fortawesome/pro-light-svg-icons'
-import { faHeart, faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import I18n from '../../../i18n/i18n'
-import CommentListModal from './comment-list-modal'
 
 class OptionPublicationModal extends React.Component {
 
@@ -25,7 +12,26 @@ class OptionPublicationModal extends React.Component {
         }
     }
 
+    _separatorItem = () => {
+        return (<View style={{ height: 1, backgroundColor: '#9b9b9b45' }} />)
+    }
+
+    _selectedAction = (code) => {
+        switch (code) {
+            case 'reportPublication': return alert('reportPublication')
+            case 'blockUser': return alert('block')
+            case 'close': return alert('close')
+        }
+    }
+
+
     render() {
+
+        const optionsList = [
+            { code: 'reportPublication', title: 'Report Publication', color: 'red' },
+            { code: 'blockUser', title: 'Block User', color: 'black' }
+        ]
+
         return (
             <View>
 
@@ -41,7 +47,27 @@ class OptionPublicationModal extends React.Component {
                     swipeDirection={'down'}
                     propagateSwipe={true}
                 >
-                    <View style={{ flex: 1, backgroundColor: 'white' }} />
+                    <View style={{ height: 'auto' }}>
+                        <FlatList
+                            scrollEnabled={false}
+                            ItemSeparatorComponent={this._separatorItem}
+                            style={{ backgroundColor: 'white', borderRadius: 15  }}
+                            data={optionsList}
+                            keyExtractor={(item) => item.code}
+                            renderItem={({ item }) =>
+                                <TouchableOpacity style={{ paddingVertical: 23, paddingHorizontal: 20, justifyContent: 'center', alignItems: 'center' }} onPress={() => this._selectedAction(item.code)}>
+                                    <Text style={{color: item.color}}>{item.title}</Text>
+                                </TouchableOpacity>
+                            }
+                        />
+                        <View style={{marginVertical: 15}}>
+                        <TouchableOpacity 
+                        onPress={() => this.props.toggleReportModal()} 
+                        style={{backgroundColor: 'white', paddingVertical: 23, paddingHorizontal: 20, borderRadius: 15, justifyContent: 'center', alignItems: 'center'}}>
+                            <Text>Close</Text>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
                 </Modal>
 
 
@@ -54,12 +80,11 @@ class OptionPublicationModal extends React.Component {
 const styles = StyleSheet.create({
     container_modal: {
         flex: 1,
-        marginHorizontal: 0,
+        marginHorizontal: 15,
         marginBottom: 0,
-        marginTop: '40%',
+        height: 'auto',
         overflow: 'hidden',
-        borderTopRightRadius: 30,
-        borderTopLeftRadius: 30
+        justifyContent: 'flex-end'
     },
 })
 
