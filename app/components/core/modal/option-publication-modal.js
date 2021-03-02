eb6107525b92de962d6ac45ec4be7276ad7ffd21
etsraphael <ets_raphael@outlook.fr>
2021-03-02 16:28:47 +1000

    update - design report 50%

--- app/components/core/modal/option-publication-modal.js
+++ app/components/core/modal/option-publication-modal.js
@@ -3,12 +3,108 @@ import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Modal from 'react-native-modal'
+import I18n from '../../../i18n/i18n'
+import { faCheckCircle } from '@fortawesome/pro-duotone-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+const optionsList = [
+    { code: 'reportPublication', title: 'Report Publication', color: 'red' },
+    { code: 'blockUser', title: 'Block User', color: 'black' }
+]
+
+const categoriesReport = [
+    {
+        number: 1,
+        type: 'REPORT.Code-Categorie.1',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.1.1',
+            'REPORT.Report-Modal-Message.Descriptions.1.2',
+            'REPORT.Report-Modal-Message.Descriptions.1.3',
+            'REPORT.Report-Modal-Message.Descriptions.1.4',
+            'REPORT.Report-Modal-Message.Descriptions.1.5',
+            'REPORT.Report-Modal-Message.Descriptions.1.6',
+            'REPORT.Report-Modal-Message.Descriptions.1.7'
+        ]
+    },
+    {
+        number: 2,
+        type: 'REPORT.Code-Categorie.2',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.2.1',
+            'REPORT.Report-Modal-Message.Descriptions.2.2',
+            'REPORT.Report-Modal-Message.Descriptions.2.3'
+        ]
+    },
+    {
+        number: 3,
+        type: 'REPORT.Code-Categorie.3',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.3.1',
+            'REPORT.Report-Modal-Message.Descriptions.3.2',
+            'REPORT.Report-Modal-Message.Descriptions.3.3'
+        ]
+    },
+    {
+        number: 4,
+        type: 'REPORT.Code-Categorie.4',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.4.1',
+            'REPORT.Report-Modal-Message.Descriptions.4.2',
+            'REPORT.Report-Modal-Message.Descriptions.4.3',
+            'REPORT.Report-Modal-Message.Descriptions.4.4',
+            'REPORT.Report-Modal-Message.Descriptions.4.5',
+            'REPORT.Report-Modal-Message.Descriptions.4.6',
+            'REPORT.Report-Modal-Message.Descriptions.4.7',
+            'REPORT.Report-Modal-Message.Descriptions.4.8'
+        ]
+    },
+    {
+        number: 5,
+        type: 'REPORT.Code-Categorie.5',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.5.1'
+        ]
+    },
+    {
+        number: 6,
+        type: 'REPORT.Code-Categorie.6',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.6.1',
+            'REPORT.Report-Modal-Message.Descriptions.6.2',
+            'REPORT.Report-Modal-Message.Descriptions.6.3'
+        ]
+    },
+    {
+        number: 7,
+        type: 'REPORT.Code-Categorie.7',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.7.1'
+        ]
+    },
+    {
+        number: 8,
+        type: 'REPORT.Code-Categorie.8',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.8.1',
+            'REPORT.Report-Modal-Message.Descriptions.8.2'
+        ]
+    },
+    {
+        number: 9,
+        type: 'REPORT.Code-Categorie.9',
+        descriptions: [
+            'REPORT.Report-Modal-Message.Descriptions.9.1',
+            'REPORT.Report-Modal-Message.Descriptions.9.2'
+        ]
+    }
+]
 
 class OptionPublicationModal extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            menu: ''
         }
     }
 
@@ -16,25 +112,145 @@ class OptionPublicationModal extends React.Component {
         return (<View style={{ height: 1, backgroundColor: '#9b9b9b45' }} />)
     }
 
-    _selectedAction = (code) => {
-        switch (code) {
-            case 'reportPublication': return alert('reportPublication')
-            case 'blockUser': return alert('block')
-            case 'close': return alert('close')
+    _defaultMenu = () => {
+        return (
+            <View>
+                <FlatList
+                    scrollEnabled={false}
+                    ItemSeparatorComponent={this._separatorItem}
+                    style={{ backgroundColor: 'white', borderRadius: 15 }}
+                    data={optionsList}
+                    keyExtractor={(item) => item.code}
+                    renderItem={({ item }) =>
+                        <TouchableOpacity
+                            style={styles.container_item_menu}
+                            onPress={() => this.setState({ menu: item.code })}
+                        >
+                            <Text style={{ color: item.color }}>{item.title}</Text>
+                        </TouchableOpacity>
+                    }
+                />
+                <View style={{ marginVertical: 15 }}>
+                    <TouchableOpacity
+                        onPress={() => this.props.toggleReportModal()}
+                        style={styles.btn_close}>
+                        <Text>{I18n.t('CORE.Close')} </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+
+    _reportPublicationView = () => {
+        return (
+            <View style={{ backgroundColor: 'white', marginBottom: 15, borderRadius: 15 }}>
+
+                {/* Header */}
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingVertical: 15 }}>
+                    <Text style={{ fontSize: 15, fontWeight: '700' }}>{I18n.t('BTN-DROPDOWN.Report')}</Text>
+                </View>
+
+                {/* Separator */}
+                {this._separatorItem()}
+
+                {/* Message */}
+                <View style={{ paddingVertical: 15, paddingHorizontal: 20 }}>
+                    <View style={{ paddingBottom: 5 }}>
+                        <Text style={{ fontSize: 15, fontWeight: '700' }}>Why are you reported this post ?</Text>
+                    </View>
+                    <View>
+                        <Text>sdfkjsnd fsdnf kdsjfndsj fndsfkj nsdfsldknfosdfnawgjpagdsfi;bgdfs godf dfpbmdgb okdfmbpdfb ndfobfd bdfbndf bojdfnb dfkjbdfbjdfngdksf jgnfdsm gdsfgn</Text>
+                    </View>
+                </View>
+
+                {this._separatorItem()}
+                <FlatList
+                    ItemSeparatorComponent={this._separatorItem}
+                    style={{ backgroundColor: 'white', borderRadius: 15, maxHeight: 400 }}
+                    data={categoriesReport}
+                    keyExtractor={(item) => item.number}
+                    renderItem={({ item }) =>
+                        <TouchableOpacity
+                            style={styles.container_item_menu}
+                            onPress={() => this.setState({ menu: 'reportPublicationSent' })}
+                        >
+                            <Text style={{ color: item.color }}>{I18n.t(item.type)}</Text>
+                        </TouchableOpacity>
+                    }
+                />
+
+
+            </View>
+        )
+    }
+
+    _blockUserView = () => {
+        return (
+            <View style={{ backgroundColor: 'white', marginBottom: 15, borderRadius: 15 }}>
+
+                {/* Header */}
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingVertical: 15 }}>
+                    <Text style={{ fontSize: 15, fontWeight: '700' }}>Block an user</Text>
+                </View>
+
+                {/* Separator */}
+                {this._separatorItem()}
+
+                {/* Message */}
+                <View style={{ paddingVertical: 15, paddingHorizontal: 20 }}>
+                    <View style={{ paddingBottom: 5 }}>
+                        <Text style={{ fontSize: 15, fontWeight: '700' }}>Are you sure to block this user ?</Text>
+                    </View>
+                    <View>
+                        <Text>sdfkjsnd fsdnf kdsjfndsj fndsfkj nsdfsldknfosdfnawgjpagdsfi;bgdfs godf dfpbmdgb okdfmbpdfb ndfobfd bdfbndf bojdfnb dfkjbdfbjdfngdksf jgnfdsm gdsfgn</Text>
+                    </View>
+                </View>
+
+                {this._separatorItem()}
+                <TouchableOpacity style={styles.container_item_menu}>
+                    <Text>No</Text>
+                </TouchableOpacity>
+
+                {this._separatorItem()}
+                <TouchableOpacity style={styles.container_item_menu}>
+                    <Text>Yes</Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+
+    _reportPublicationSentView = () => {
+        return (
+            <View style={{ backgroundColor: 'white', marginBottom: 15, borderRadius: 15 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingVertical: 35 }}>
+                    <FontAwesomeIcon icon={faCheckCircle} color={'#33cc33'} secondaryColor={'#f2f2f2'} size={75} />
+                </View>
+                {this._separatorItem()}
+                <View style={{paddingHorizontal: 15, paddingVertical: 10, marginBottom: 30}}>
+                    <Text>sdfkjsnd fsdnf kdsjfndsj fndsfkj nsdfsldknfosdfnawgjpagdsfi;bgdfs godf dfpbmdgb okdfmbpdfb ndfobfd bdfbndf bojdfnb dfkjbdfbjdfngdksf jgnfdsm gdsfgn</Text>
+                </View>
+            </View>
+        )
+    }
+
+    _blockUserSentView = () => {
+        return null
+    }
+
+    _displaySection = () => {
+        switch (this.state.menu) {
+            case 'reportPublication': return this._reportPublicationView()
+            case 'blockUser': return this._blockUserView()
+            case 'reportPublicationSent': return this._reportPublicationSentView()
+            case 'blockUserSent': return this._blockUserSentView()
+            default: return this._defaultMenu()
         }
     }
 
-
     render() {
-
-        const optionsList = [
-            { code: 'reportPublication', title: 'Report Publication', color: 'red' },
-            { code: 'blockUser', title: 'Block User', color: 'black' }
-        ]
-
         return (
             <View>
-
                 <Modal
                     onSwipeComplete={() => this.props.toggleReportModal()}
                     onBackdropPress={() => this.props.toggleReportModal()}
@@ -48,25 +264,7 @@ class OptionPublicationModal extends React.Component {
                     propagateSwipe={true}
                 >
                     <View style={{ height: 'auto' }}>
-                        <FlatList
-                            scrollEnabled={false}
-                            ItemSeparatorComponent={this._separatorItem}
-                            style={{ backgroundColor: 'white', borderRadius: 15  }}
-                            data={optionsList}
-                            keyExtractor={(item) => item.code}
-                            renderItem={({ item }) =>
-                                <TouchableOpacity style={{ paddingVertical: 23, paddingHorizontal: 20, justifyContent: 'center', alignItems: 'center' }} onPress={() => this._selectedAction(item.code)}>
-                                    <Text style={{color: item.color}}>{item.title}</Text>
-                                </TouchableOpacity>
-                            }
-                        />
-                        <View style={{marginVertical: 15}}>
-                        <TouchableOpacity 
-                        onPress={() => this.props.toggleReportModal()} 
-                        style={{backgroundColor: 'white', paddingVertical: 23, paddingHorizontal: 20, borderRadius: 15, justifyContent: 'center', alignItems: 'center'}}>
-                            <Text>Close</Text>
-                        </TouchableOpacity>
-                        </View>
+                        {this._displaySection()}
                     </View>
                 </Modal>
 
@@ -86,6 +284,20 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         justifyContent: 'flex-end'
     },
+    container_item_menu: {
+        paddingVertical: 23,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btn_close: {
+        backgroundColor: 'white',
+        paddingVertical: 23,
+        paddingHorizontal: 20,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 })
 
 const mapStateToProps = state => ({
