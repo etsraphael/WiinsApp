0f4adf27c086465b0d41abea8ecbf1b1a6baf510
etsraphael <ets_raphael@outlook.fr>
2021-03-16 14:56:31 +1000

    saveTokenDevice created

---
+++ app/services/notification/token-service.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import { sendError } from './../../../app/services/error/error-service'
+
+export async function saveTokenDevice(device) {
+        const token = await AsyncStorage.getItem('userToken')
+
+        try {
+                return fetch('https://wiins-backend.herokuapp.com/user/token/setting', {
+                        method: 'POST',
+                        headers: {
+                                Accept: 'application/json', 'Content-Type': 'application/json',
+                                'Authorization': 'Bearer ' + token,
+                        },
+                        body: JSON.stringify({ device })
+                })
+                        .then((response) => response.json())
+                        .then(console.log)
+        } catch (error) {
+                return sendError(error)
+        }
+
+}
