c150705d8c5447b67bb0908a101216045db2a1fd
etsraphael <ets_raphael@outlook.fr>
2021-03-19 11:57:35 +1000

    udapte with go back in the camera

--- app/navigation/publication-navigation.js
+++ app/navigation/publication-navigation.js
@@ -6,12 +6,15 @@ import MyStory from '../components/space/publication/my-story'
 
 const Stack = createStackNavigator()
 
-export default PublicationModalNavigation = () => (
+export default PublicationModalNavigation = (propsInitial) => (
+
     <Stack.Navigator
         screenOptions={{ headerShown: false }}
         initialRouteName={'Camera'}
     >
-        <Stack.Screen name={'Camera'} component={Camera} />
+        <Stack.Screen name={'Camera'}>
+            {() => <Camera closeModal={propsInitial.closeModal} />}
+        </Stack.Screen>
         <Stack.Screen name={'PendingPublication'} component={PendingPublication} />
         <Stack.Screen name={'MyStory'} component={MyStory} />
     </Stack.Navigator>
