3a34cbeed03ef5097b05c7ab1eeb0fc16316c62f
etsraphael <ets_raphael@outlook.fr>
2021-04-13 15:44:43 +1000

    design notification done

--- app/components/space/feed/notification/notification-page.js
+++ app/components/space/feed/notification/notification-page.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import { StyleSheet, View, FlatList, TouchableOpacity, Image, SafeAreaView, VirtualizedList, Text } from 'react-native'
+import { StyleSheet, SafeAreaView, VirtualizedList, View, TouchableOpacity, Text } from 'react-native'
 import { connect } from 'react-redux'
 import * as PublicationFeedActions from '../../../../redux/FeedPublications/actions'
 import * as SearchActions from '../../../../redux/SearchBar/actions'
 import * as PublicationInModalActions from '../../../../redux/PublicationInModal/actions'
 import * as StoriesActions from '../../../../redux/Stories/actions'
 import { bindActionCreators } from 'redux'
+import OneNotification from './one-notification'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faAngleLeft } from '@fortawesome/pro-solid-svg-icons'
+
+
+const notificationList = [
+    { _id: 1, type: 'NotificationComment', read: true },
+    { _id: 2, type: 'NotificationResponse', read: true },
+    { _id: 3, type: 'NotificationLike', read: false },
+    { _id: 4, type: 'NotificationCommentLike', read: true },
+    { _id: 5, type: 'NotificationCommentLikePlaylist', read: true },
+    { _id: 6, type: 'NotificationCommentResponsePlaylist', read: true },
+    { _id: 7, type: 'NotificationRequest', read: true },
+    { _id: 8, type: 'NotificationTagCommentPublication', read: true },
+    { _id: 9, type: 'NotificationTagCommentPlaylist', read: true },
+    { _id: 10, type: 'NotificationTagPublication', read: true },
+    { _id: 11, type: 'NotificationFeatPublication', read: true },
+    { _id: 12, type: 'NotificationReport', read: true },
+    { _id: 13, type: 'NotificationPageReport', read: true },
+    { _id: 14, type: 'NotificationVerification', read: true },
+    { _id: 15, type: 'NotificationCertification', read: true },
+]
 
 class NotificationPage extends React.Component {
 
@@ -16,7 +38,29 @@ class NotificationPage extends React.Component {
     render = () => {
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <Text>Notication page</Text>
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', padding: 15 }}>
+                    <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{ paddingHorizontal: 15 }}>
+                        <FontAwesomeIcon icon={faAngleLeft} color={'black'} size={35} />
+                    </TouchableOpacity>
+                    <View>
+                        <Text style={{ fontSize: 30, fontWeight: '700' }}>Notification</Text>
+                    </View>
+                </View>
+
+                <VirtualizedList
+                    onRefresh={null}
+                    refreshing={false}
+                    showsVerticalScrollIndicator={false}
+                    style={{ marginBottom: 50 }}
+                    data={notificationList}
+                    renderItem={({ item, index }) => <OneNotification notification={item[index]} />}
+                    keyExtractor={(item, index) => item[index]._id}
+                    getItemCount={() => notificationList.length}
+                    getItem={(data) => data}
+                    scrollEventThrottle={5}
+                    onMomentumScrollEnd={() => null}
+                />
             </SafeAreaView>
         )
     }
@@ -24,10 +68,7 @@ class NotificationPage extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    feed_container: {
-        flex: 1,
-        backgroundColor: '#eef2f4'
-    }
+
 })
 
 const mapStateToProps = state => ({
---
+++ app/components/space/feed/notification/one-notification.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { StyleSheet, View, FlatList, TouchableOpacity, Image, SafeAreaView, VirtualizedList, Text } from 'react-native'
+import { connect } from 'react-redux'
+import * as PublicationFeedActions from '../../../../redux/FeedPublications/actions'
+import * as SearchActions from '../../../../redux/SearchBar/actions'
+import * as PublicationInModalActions from '../../../../redux/PublicationInModal/actions'
+import * as StoriesActions from '../../../../redux/Stories/actions'
+import { bindActionCreators } from 'redux'
+import FastImage from 'react-native-fast-image'
+import { faEllipsisH } from '@fortawesome/pro-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+class OneNotification extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    _activeBackground = (read) => {
+        if (read) return { backgroundColor: '#eef2f4' }
+        else return { backgroundColor: '#cce0ff' }
+    }
+
+    _oneNotificationRender = (item) => {
+        return (
+            <View style={[{ flexDirection: 'row', paddingVertical: 9 }, this._activeBackground(item.read)]}>
+
+                {/* Picture profile notification */}
+                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+                    <FastImage
+                        style={{ width: 70, height: 70, borderRadius: 70, resizeMode: 'cover' }}
+                        source={{ uri: 'https://images.unsplash.com/photo-1617961940214-ae863cb58f1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80', priority: FastImage.priority.normal }}
+                        resizeMode={FastImage.resizeMode.cover}
+                    />
+                </View>
+
+                {/* Description */}
+                <View style={{ flex: 5, justifyContent: 'center', paddingHorizontal: 15 }}>
+                    <Text style={{ fontSize: 15 }}> id: {item._id} sdfkjdsnf dsflkmds fljsdnfdsljkndsbdsfsdnf dsljfnsd fsdb fmjbfkds fmhdsf bdsmhfk</Text>
+                </View>
+
+                {/* Content Publication */}
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <TouchableOpacity>
+                        <FontAwesomeIcon icon={faEllipsisH} color={'grey'} size={19} />
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        )
+    }
+
+    render = () => {
+        switch (this.props.notification.type) {
+            default: return this._oneNotificationRender(this.props.notification)
+        }
+    }
+
+}
+
+const styles = StyleSheet.create({
+    feed_container: {
+        flex: 1,
+        backgroundColor: '#eef2f4'
+    }
+})
+
+const mapStateToProps = state => ({
+    FeedPublications: state.FeedPublications,
+    MyUser: state.MyUser,
+    SearchList: state.Search,
+    MyProfile: state.MyProfile,
+    Stories: state.Stories
+})
+
+const ActionCreators = Object.assign(
+    {},
+    PublicationFeedActions,
+    SearchActions,
+    PublicationInModalActions,
+    StoriesActions
+)
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(ActionCreators, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(OneNotification)
