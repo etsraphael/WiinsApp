63811023a147675943490585806a34fd87c055e6
etsraphael <ets_raphael@outlook.fr>
2021-04-14 11:04:52 +1000

    notification type added

--- app/components/space/feed/notification/one-notification.js
+++ app/components/space/feed/notification/one-notification.js
@@ -22,35 +22,10 @@ class OneNotification extends React.Component {
         else return { backgroundColor: '#cce0ff' }
     }
 
-    _oneNotificationRender = (item) => {
-        return (
-            <View style={[{ flexDirection: 'row', paddingVertical: 9 }, this._activeBackground(item.read)]}>
-
-                {/* Picture profile notification */}
-                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-                    <FastImage
-                        style={{ width: 70, height: 70, borderRadius: 70, resizeMode: 'cover' }}
-                        source={{ uri: 'https://images.unsplash.com/photo-1617961940214-ae863cb58f1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80', priority: FastImage.priority.normal }}
-                        resizeMode={FastImage.resizeMode.cover}
-                    />
-                </View>
-
-                {/* Description */}
-                <View style={{ flex: 5, justifyContent: 'center', paddingHorizontal: 15 }}>
-                    <Text style={{ fontSize: 15 }}> sdfkjdsnf dsflkmds fljsdnfdsljkndsbdsfsdnf dsljfnsd fsdb fmjbfkds fmhdsf bdsmhfk</Text>
-                </View>
-
-                {/* Content Publication */}
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <TouchableOpacity>
-                        <FontAwesomeIcon icon={faEllipsisH} color={'grey'} size={19} />
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-        )
+    _notificationToCode = (item) => {
+        console.log(item)
+        return (<View />)
     }
-
 
     _notificationCommentFeedRender = (notif) => {
         return (
@@ -67,6 +42,7 @@ class OneNotification extends React.Component {
 
                 {/* Content Publication */}
                 {this._ellipsiBtnRender()}
+
             </View>
         )
     }
@@ -82,6 +58,26 @@ class OneNotification extends React.Component {
                 <View style={{ flex: 5, justifyContent: 'center', paddingHorizontal: 15 }}>
                     {notif.accepted && <Text style={{ fontSize: 15 }}>You have been verified in the platform. You can enjoy all the extras now. </Text>}
                     {!notif.accepted && <Text style={{ fontSize: 15 }}>You havn't been verified due to your requirements. Try a new time when you're ready</Text>}
+                    <Text style={{ fontSize: 13, color: 'grey', paddingTop: 2 }}>{getDateTranslated(notif.updatedAt)}</Text>
+                </View>
+
+                {/* Content Publication */}
+                {this._ellipsiBtnRender()}
+
+            </View>
+        )
+    }
+
+    _notificationCommentLikePlaylistRender = (notif) => {
+        return (
+            <View style={[{ flexDirection: 'row', paddingVertical: 9 }, this._activeBackground(notif.read)]}>
+
+                {/* Picture profile notification */}
+                {this._avatarNotificationRender(notif.profile.pictureprofile)}
+
+                {/* Description */}
+                <View style={{ flex: 5, justifyContent: 'center', paddingHorizontal: 15 }}>
+                    <Text style={{ fontSize: 15 }}>{notif.profile._meta.pseudo} like your comment in a music playlist </Text>
                     <Text style={{ fontSize: 13, color: 'grey', paddingTop: 2 }}>{getDateTranslated(notif.updatedAt)}</Text>
                 </View>
 
@@ -118,7 +114,19 @@ class OneNotification extends React.Component {
         switch (this.props.notification.type) {
             case 'NotificationVerification': return this._verificationRender(this.props.notification)
             case 'NotificationComment': return this._notificationCommentFeedRender(this.props.notification)
-            default: return this._oneNotificationRender(this.props.notification)
+            case 'NotificationCommentLikePlaylist': return this._notificationCommentLikePlaylistRender(this.props.notification)
+            case 'NotificationLike':
+            case 'NotificationResponse':
+            case 'NotificationCommentLike':
+            case 'NotificationCommentResponsePlaylist':
+            case 'NotificationTagCommentPublication':
+            case 'NotificationTagCommentPlaylist':
+            case 'NotificationTagPublication':
+            case 'NotificationFeatPublication':
+            case 'NotificationReport':
+            case 'NotificationPageReport':
+            case 'NotificationCertification':
+            default: return this._notificationToCode(this.props.notification)
         }
     }
 
