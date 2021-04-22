5ad384792c9a941e230395720ca0bdcb423f8b49
etsraphael <ets_raphael@outlook.fr>
2021-04-22 14:59:01 +1000

    open librairy file is working from the verification

--- app/components/space/setting/setting-certification.js
+++ app/components/space/setting/setting-certification.js
@@ -3,12 +3,13 @@ import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from 'react-nati
 import { connect } from 'react-redux'
 import * as MyUserActions from '../../../redux/MyUser/actions'
 import { bindActionCreators } from 'redux'
-import { faArrowLeft, faDownload } from '@fortawesome/pro-duotone-svg-icons'
+import { faArrowLeft, faDownload, faTreeChristmas } from '@fortawesome/pro-duotone-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import i18n from '../../../../assets/i18n/i18n'
 import CheckBox from '@react-native-community/checkbox'
 import LinearGradient from 'react-native-linear-gradient'
+import { launchImageLibrary } from 'react-native-image-picker'
 
 class SettingCertification extends React.Component {
 
@@ -16,7 +17,10 @@ class SettingCertification extends React.Component {
         super(props)
         this.state = {
             pageSelected: 'v',
-            verifTerm: true
+            verifTerm: true,
+            idRectoReceived: false,
+            idVersoReceived: false,
+            facePhotoReceived: false,
         }
     }
 
@@ -90,10 +94,29 @@ class SettingCertification extends React.Component {
     }
 
     _saveFile = (type) => {
+
+        const options = { storageOptions: { skipBackup: true, path: 'images' } }
+
+        launchImageLibrary(options, (response) => {
+            if (!response.uri) return null
+            else return this._sendFile(response.uri, type)
+        })
+
+    }
+
+
+    _sendFile = (file, type) => {
+
         switch (type) {
-            case 'id-recto': return null
-            case 'id-verso': return null
-            case 'face-photo': return null
+            case 'id-recto': {
+                return this.setState({idRectoReceived: true})
+            }
+            case 'id-verso': {
+                return this.setState({idVersoReceived: true})
+            }
+            case 'face-photo': {
+                return this.setState({facePhotoReceived: true})
+            }
         }
     }
 
@@ -103,21 +126,30 @@ class SettingCertification extends React.Component {
 
                 <Text style={{ textAlign: 'center' }}>{i18n.t('SETTING.verified.Pl-s-t-docs-a-complet-t-form-below')}</Text>
 
-                <TouchableOpacity style={styles.container_section} onPress={() => this._saveFile('id-recto')}>
+                <TouchableOpacity
+                    style={styles.container_section(this.state.idRectoReceived)}
+                    onPress={() => this._saveFile('id-recto')}
+                >
                     {this._uploadIconAndTextRender()}
                     <View style={{ paddingHorizontal: 15 }}>
                         <Text>{i18n.t('SETTING.verified.Upload-yr-password-id-identity-or-driver-license')}</Text>
                     </View>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.container_section} onPress={() => this._saveFile('id-verso')}>
+                <TouchableOpacity
+                    style={styles.container_section(this.state.idVersoReceived)}
+                    onPress={() => this._saveFile('id-verso')}
+                >
                     {this._uploadIconAndTextRender()}
                     <View style={{ paddingHorizontal: 15 }}>
                         <Text>{i18n.t('SETTING.verified.M-s-t-upload-t-back-of-the-card-if-its-a-national-id-card')}</Text>
                     </View>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.container_section} onPress={() => this._saveFile('face-photo')}>
+                <TouchableOpacity
+                    style={styles.container_section(this.state.facePhotoReceived)}
+                    onPress={() => this._saveFile('face-photo')}
+                >
                     {this._uploadIconAndTextRender()}
                     <View style={{ paddingHorizontal: 15 }}>
                         <Text>{i18n.t('SETTING.verified.Upload-a-photo-holding-t-ID-D')}</Text>
@@ -186,13 +218,13 @@ const styles = StyleSheet.create({
         fontSize: 19,
         fontWeight: '600'
     },
-    container_section: {
+    container_section: (actif) => ({
         marginVertical: 15,
         paddingVertical: 15,
         paddingHorizontal: 10,
-        backgroundColor: '#d9d9d9',
+        backgroundColor: actif ? '#c2f0c2' : '#d9d9d9',
         borderRadius: 10
-    }
+    })
 })
 
 const mapStateToProps = state => ({
