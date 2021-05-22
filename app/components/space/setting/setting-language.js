a8b23492e801f5b9d42c47c05a65145b488a49ce
etsraphael <ets_raphael@outlook.fr>
2021-05-22 13:32:17 +1000

    design created to set the language

--- app/components/space/setting/setting-language.js
+++ app/components/space/setting/setting-language.js
@@ -1,22 +1,71 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import * as MyUserActions from '../../../redux/MyUser/actions'
 import { bindActionCreators } from 'redux'
+import { Picker } from '@react-native-picker/picker';
+import i18n from '../../../../assets/i18n/i18n'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faArrowLeft } from '@fortawesome/pro-duotone-svg-icons'
+import { languageList } from './../../core/data/language'
 
 class SettingLanguage extends React.Component {
 
+
     constructor(props) {
         super(props)
+        this.state = {
+            languageSelected: null
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({ languageSelected: this.props.MyUser.user.config.language })
+    }
+
+    _renderHeader = () => {
+        return (
+            <View>
+                <View style={{ flexDirection: 'row', padding: 15 }}>
+                    <TouchableOpacity onPress={() => this.props.navigation.goBack()}
+                        style={{ justifyContent: 'center', alignItems: 'center' }}>
+                        <FontAwesomeIcon icon={faArrowLeft} color={'grey'} size={30} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{ fontSize: 19, fontWeight: '600' }}>{i18n.t('SETTING.lang.lang-of-plateforme')}</Text>
+                </View>
+
+            </View>
+        )
+    }
+
+    _pickerView = () => {
+        return <Picker
+            selectedValue={this.state.languageSelected}
+            onValueChange={(itemValue) => this.setState({ languageSelected: itemValue })}>
+            {languageList.map(x => <Picker.Item label={x.language} value={x.code} />)}
+        </Picker>
+    }
+
+    _confirmBtnView = () => {
+        return (
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <TouchableOpacity style={styles.container_confirm_btn}>
+                    <Text style={styles.text_confirm}>Confirm</Text>
+                </TouchableOpacity>
+            </View>
+        )
     }
 
     render() {
         return (
             <View style={styles.main_container}>
-                <Text> Setting Language </Text>
-
-
-                <View style={{flexDirection: 'row'}}></View>
+                {this._renderHeader()}
+                {this._pickerView()}
+                {this.props.MyUser.user.config.language !== this.state.languageSelected && this._confirmBtnView()}
             </View>
         )
     }
@@ -25,8 +74,22 @@ class SettingLanguage extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
+        paddingTop: Platform.OS === 'ios' ? getStatusBarHeight() : 0,
+        marginBottom: 65
+    },
+    container_confirm_btn: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginHorizontal: 5,
+        borderRadius: 8,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#6600ff'
+    },
+    text_confirm: {
+        fontSize: 19,
+        fontWeight: '600',
+        color: 'white'
     }
 })
 
