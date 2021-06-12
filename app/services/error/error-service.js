4c5836d5832455c51a79359c28cece5b0e504bfd
etsraphael <ets_raphael@outlook.fr>
2021-06-12 11:15:45 +1000

    sentry updated

--- app/services/error/error-service.js
+++ app/services/error/error-service.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react-native'
 
 export function initSentry() {
     return Sentry.init({
-        dsn: 'https://61507dd63297425f8ce6a729128a91b4@o485463.ingest.sentry.io/5655642',
+        dsn: 'https://e718b35c2d1c4f5cbb44584ef644e3aa@o833200.ingest.sentry.io/5812922',
     })
 }
 
