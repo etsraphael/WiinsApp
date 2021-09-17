088d3102f33802df7828dbc06529b4790bd560cf
etsraphael <ets_raphael@outlook.fr>
2021-09-17 22:03:12 +1000

    input label updated

--- app/components/core/reusable/form/inputs.js
+++ app/components/core/reusable/form/inputs.js
@@ -6,29 +6,35 @@ import LinearGradient from "react-native-linear-gradient";
 import { Theme } from "../design";
 
 
-export const StandardCustomBtn = ({ children, fillWidth=false, style, ...rest }) => (
-     <TouchableOpacity {...rest} style={[ style, Styles.wButton, Styles.wCornerRadius ]}>
-          { children }
+export const StandardCustomBtn = ({ children, fillWidth = false, style, ...rest }) => (
+     <TouchableOpacity {...rest} style={[style, { paddingVertical: 10, borderRadius: 15 }]}>
+          {children}
      </TouchableOpacity>
 );
 
-export const WInput = ({ inputRef, label, boxStyle, style, placeholderStyle, flag=false, ...rest }) => (
+export const WInput = ({ inputRef, label, boxStyle, style, placeholderStyle, flag = false, ...rest }) => (
      <View style={boxStyle}>
-          { label && <Text style={[Styles.wInputLabel, { marginBottom: 9 }]}>{ label }</Text> }
-          <TextInput ref={inputRef} style={[style, placeholderStyle, Styles.wInput, Styles.wCornerRadius, (flag && Styles.wInputFlagged)]} placeholderTextColor="#C1C7D0" selectionColor='#002251' { ...rest } />
+          {label && <Text style={[{
+               marginBottom: 9, fontSize: 14,
+               color: "#C1C7D0"
+          }]}>{label}</Text>}
+          <TextInput ref={inputRef} style={[style, placeholderStyle, Styles.wInput, { borderRadius: 15 }, (flag && Styles.wInputFlagged)]} placeholderTextColor="#C1C7D0" selectionColor='#002251' {...rest} />
      </View>
 )
 
-export const StandardInputPassword = ({ inputRef, label, boxStyle, style, placeholderStyle, flag=false, ...rest }) => {
+export const StandardInputPassword = ({ inputRef, label, boxStyle, style, placeholderStyle, flag = false, ...rest }) => {
      const [visible, setVisible] = React.useState(false);
      return (
           <View style={boxStyle}>
-               { label && <Text style={[Styles.wInputLabel, { marginBottom: 9 }]}>{ label }</Text> }
+               {label && <Text style={[{
+                    marginBottom: 9, fontSize: 14,
+                    color: "#C1C7D0"
+               }]}>{label}</Text>}
                <View>
-                    <TextInput ref={inputRef} textContentType={visible ? "none": "password"} secureTextEntry={!visible} style={[style, placeholderStyle, Styles.wInput, Styles.wCornerRadius, Styles.pwdInput, (flag && Styles.wInputFlagged)]} placeholderTextColor="#C1C7D0" selectionColor='#002251' { ...rest } />
+                    <TextInput ref={inputRef} textContentType={visible ? "none" : "password"} secureTextEntry={!visible} style={[style, placeholderStyle, Styles.wInput, { borderRadius: 15 }, Styles.pwdInput, (flag && Styles.wInputFlagged)]} placeholderTextColor="#C1C7D0" selectionColor='#002251' {...rest} />
                     <View style={Styles.pwdIconBox}>
                          <TouchableOpacity onPress={() => setVisible(!visible)}>
-                              <FontAwesomeIcon icon={!visible ? faEyeSlash : faEye} size={25} color={Theme.wIconTint}/>
+                              <FontAwesomeIcon icon={!visible ? faEyeSlash : faEye} size={25} color={Theme.wIconTint} />
                          </TouchableOpacity>
                     </View>
                </View>
@@ -36,53 +42,42 @@ export const StandardInputPassword = ({ inputRef, label, boxStyle, style, placeh
      )
 }
 
-export const WButton = ({ text, fillWidth=false, style, ...rest }) => (
-     <TouchableOpacity {...rest} style={[ style, Styles.wButton, Styles.wCornerRadius ]}>
-          <Text style={Styles.wButtonText}>{ text }</Text>
+export const WButton = ({ text, fillWidth = false, style, ...rest }) => (
+     <TouchableOpacity {...rest} style={[style, { paddingVertical: 10, borderRadius: 15 }]}>
+          <Text style={Styles.buttonText}>{text}</Text>
      </TouchableOpacity>
 )
 
-export const PrimaryGradientButton = ({ text, fillWidth=false, style, ...rest }) => (
+export const PrimaryGradientButton = ({ text, fillWidth = false, style, ...rest }) => (
      <TouchableOpacity {...rest}>
           <LinearGradient
-               colors={['#00DAFA','#3087D7','#6743E0']}
+               colors={['#00DAFA', '#3087D7', '#6743E0']}
                location={[0, 0.5217208328780595, 1]}
                start={{ x: 0, y: 0 }}
                end={{ x: 1, y: 1 }}
-               style={[ style, Styles.wButton, Styles.wCornerRadius, { flex: 1 } ]}
+               style={[style, { flex: 1, paddingVertical: 10, borderRadius: 15 }]}
           >
-               <Text style={Styles.wButtonText}>{ text }</Text>
+               <Text style={Styles.buttonText}>{text}</Text>
           </LinearGradient>
      </TouchableOpacity>
 )
 
 
 export const Styles = new StyleSheet.create({
-     wButton: {
-          paddingVertical: 10,
-     },
-     wButtonText: {
+     buttonText: {
           color: '#ffffff',
           fontSize: 16,
           textAlign: 'center'
      },
-     wCornerRadius: {
-          borderRadius: 15
-     },
-     wInputLabel: {
-          fontSize: 14,
-          color: "#C1C7D0"
-     },
      wInput: {
           backgroundColor: "#F2F4F7",
           paddingHorizontal: 16,
-          // paddingVertical: 14,
           height: 46,
           width: "100%",
           color: "#002251",
           fontSize: 16,
      },
      wInputFlagged: { borderColor: 'red', borderWidth: 2 },
-     pwdInput: {  paddingRight: 54 },
+     pwdInput: { paddingRight: 54 },
      pwdIconBox: { position: "absolute", right: 0, height: "100%", width: 45, justifyContent: "center" }
 });
